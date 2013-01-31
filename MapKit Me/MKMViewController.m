802640676abb9c4097978b5e6df0e580d5a51a98
Justin R. Miller <incanus@codesorcery.net>
2013-01-30 18:17:10 -0800

    add pin drop

--- MapKit Me/MKMViewController.m
+++ MapKit Me/MKMViewController.m
@@ -10,7 +10,7 @@
 
 #import <MapKit/MapKit.h>
 
-@interface MKMViewController ()
+@interface MKMViewController () <MKMapViewDelegate>
 
 @property (nonatomic, strong) IBOutlet MKMapView *mapView;
 @property (nonatomic, strong) IBOutlet UISegmentedControl *segmentedControl;
@@ -33,6 +33,11 @@
     self.navigationItem.rightBarButtonItem = [[MKUserTrackingBarButtonItem alloc] initWithMapView:self.mapView];
     
     self.mapView.showsUserLocation = YES;
+
+    self.mapView.delegate = self;
+
+    [self.mapView addGestureRecognizer:[[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)]];
+    [self.mapView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)]];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -57,6 +62,48 @@
         self.mapView.mapType = MKMapTypeHybrid;
     else if (sender.selectedSegmentIndex == 2)
         self.mapView.mapType = MKMapTypeSatellite;
+}
+
+- (void)handleLongPress:(UILongPressGestureRecognizer *)recognizer
+{
+    if (recognizer.state != UIGestureRecognizerStateBegan)
+        return;
+
+    for (id <MKAnnotation>annotation in self.mapView.annotations)
+        if ( ! [annotation isKindOfClass:[MKUserLocation class]])
+            [self.mapView removeAnnotation:annotation];
+
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+
+    annotation.coordinate = [self.mapView convertPoint:[recognizer locationInView:self.mapView] toCoordinateFromView:self.mapView];
+
+    annotation.title = @"Dropped Pin";
+
+    [self.mapView addAnnotation:annotation];
+}
+
+- (void)handleSingleTap:(UITapGestureRecognizer *)recognizer
+{
+    if ([self.mapView.annotations count] > 1)
+        for (id <MKAnnotation>annotation in self.mapView.annotations)
+            if ( ! [annotation isKindOfClass:[MKUserLocation class]])
+                [self.mapView removeAnnotation:annotation];
+}
+
+#pragma mark -
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    if ([annotation isEqual:self.mapView.userLocation])
+        return nil;
+
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+
+    pin.animatesDrop = YES;
+
+    pin.canShowCallout = YES;
+
+    return pin;
 }
 
 @end
